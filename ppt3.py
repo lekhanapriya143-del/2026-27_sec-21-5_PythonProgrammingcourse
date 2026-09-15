95f0b38a735f03036ecaf7ef967f9601d840b3f5
lekhanapriya143-del <lekhanapriya143@gmail.com>
2026-09-15 13:25:27 +0530

    Add files via upload

---
+++ ppt3.py
@@ -0,0 +1,27 @@
+int_num =10
+float_num=float(int_num)
+print(float_num)
+print(type(float_num))
+
+
+
+
+int_num=10
+str_num=str(int_num)
+print(str_num)
+print(type(str_num))
+
+
+
+float_num=10.5
+int_num=int(float_num)
+print(int_num)
+print(type(int_num))
+
+
+
+
+float_num=10.5
+str_num=str(float_num)
+print(str_num)
+print(type(str_num))
