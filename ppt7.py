f294cfdee1beb919030b876a620fcbf2e5f8e3d0
lekhanapriya143-del <lekhanapriya143@gmail.com>
2026-09-15 13:57:01 +0530

    Add files via upload

---
+++ ppt7.py
@@ -0,0 +1,16 @@
+principal=float(input("Enter the principal amount: "))
+rate=float(input("Enter the rate of interest: "))  
+time=float(input("Enter the time period: "))
+simple_interest=(principal*rate*time)/100
+print("The simple interest is: ",simple_interest)
+
+
+
+
+
+principal = float(input("Enter principal: "))
+rate = float(input("Enter rate: "))
+time = float(input("Enter time: "))
+amount = principal * (1 + rate / 100) ** time
+compound_intrest = amount - principal - amount
+print("Compound Intrest=",compound_intrest)
